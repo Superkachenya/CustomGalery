054070091f1b7a89a9494367a10e9ee7816ac7c2
Danil <voitenko.cr@gmail.com>
2016-03-11 17:26:01 +0200

    create CustomGallery;

---
+++ CustomGallery-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomGallery-iOS
+//
+//  Created by Danil on 11.03.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
